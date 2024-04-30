fcc49c746db106eb79271ffec5f9d602868c3598
sajmonkon <144121951+sajmonkon@users.noreply.github.com>
2024-04-30 13:03:31 +0200

    Add files via upload

---
+++ html projekt/kontakty.php
@@ -0,0 +1,41 @@
+<html>
+<head><!-- Google tag (gtag.js) -->
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>kontakty</title>
+    <link rel="icon" type="image/x-icon" href="bodybuildinglogo.jpg">
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='styly.css'>
+    <script src='main.js'></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-CJKP4W9R55"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-CJKP4W9R55');
+</script>
+</head>
+    <style>
+        body {
+            background-image: url('Prezentace1.jpg');
+            background-repeat: no-repeat;
+            background-size: cover;
+            /* Additional background properties if needed */
+        }
+
+        /* Add your other CSS styles here */
+    </style>
+
+<header> 
+ <ul>
+        <li><a href="index.html">MENU</a></li>
+        <li class="dropdown"></header>
+
+
+
+<footer>
+    <p style="color: white; text-align: right;">&copy; 2023 Kontakty Website. All rights reserved.</p>
+</footer>
+
+</html>
